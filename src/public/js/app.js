718dc198d9f8424c48f643dbac1d40a64a79efd8
runan13 <tjrrh123@gmail.com>
2021-08-17 19:44:13 +0900

    WebSocket - Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,23 @@
-alert("test!");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+const handleOpen = () => {
+  console.log("Connected to Server 🚀");
+};
+
+const handleMessage = (message) => {
+  console.log("New Message : ", message.data);
+};
+
+const handleClose = () => {
+  console.log("Disconnected to Server 🤑");
+};
+
+socket.addEventListener("open", handleOpen);
+
+socket.addEventListener("message", handleMessage);
+
+socket.addEventListener("close", handleMessage);
+
+setTimeout(() => {
+  socket.send("Hello From Browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -13,7 +13,21 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
+
+const onSocketClose = () => {
+  console.log("Disconnected to Client 🤑");
+};
+
+const onSocketMessage = (message) => {
+  console.log(message);
+};
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Server 🚀");
+  socket.on("close", onSocketClose);
+  socket.on("message", onSocketMessage);
+  socket.send("hello!");
+});
 
 server.listen(3000, handleListen);
