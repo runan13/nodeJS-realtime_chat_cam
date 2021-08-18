b15cc8987e7743fe7526c19253016209e306ffe7
runan13 <tjrrh123@gmail.com>
2021-08-18 19:53:12 +0900

    SocketIO - Rooms

--- src/server.js
+++ src/server.js
@@ -18,7 +18,13 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.onAny((event) => {
+    console.log(`Socket Event : ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+  });
 });
 
 // const onSocketClose = () => {
